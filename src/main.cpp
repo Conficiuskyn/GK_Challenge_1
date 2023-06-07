1bb9dd225619ce0f751b75d21500624dbe72b704
Conficiuskyn <Moustaphcsk@gmail.com>
2023-06-08 01:12:49 +0200

    basic main app

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+class Main
+{
+    const char* TAG = "[main]";
+    public:
+        void run()
+        {   
+            while(true)
+            {
+                ESP_LOGI(TAG, "Infinite loop");
+                vTaskDelay(pdMS_TO_TICKS(1000));
+            }
+        }
+};
+
+
 extern "C" void app_main(void)
 {
-
+    Main app;
+    app.run();
 }
