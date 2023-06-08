1cfe1476993bb24c3149784ce24e348d67bd9ef4
Conficiuskyn <Moustaphcsk@gmail.com>
2023-06-08 03:00:23 +0200

    writing and reading request are done

--- include/components/http_server.h
+++ include/components/http_server.h
@@ -1,5 +1,6 @@
 #include "esp_http_server.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
 
 class HttpServer 
 {
@@ -10,6 +11,7 @@ class HttpServer
         static constexpr const char* TAG = "[http_server]";
     
     private:
+        static constexpr const char* _EEPROM_KEY = "0x123";
         bool _running{false};
         httpd_handle_t _server;
         static esp_err_t _writeStringToEEPROM(httpd_req_t *req);
--- src/components/http_server.cpp
+++ src/components/http_server.cpp
@@ -25,10 +25,41 @@ bool HttpServer::isRunning() {
 }
 
 esp_err_t HttpServer::_writeStringToEEPROM(httpd_req_t *req) 
-{
+{   
+    char content[200];
+    int content_length = httpd_req_get_hdr_value_len(req, "Content-Length") + 1;
+    if (content_length > sizeof(content)) {
+        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Content too long");
+        return ESP_FAIL;
+    }
+    httpd_req_recv(req, content, content_length);
+    content[content_length] = '\0';
+    httpd_resp_sendstr(req, "POST request received");
+    ESP_LOGI(TAG, "Received data: %s", content);
 
+    nvs_handle_t nvs_handler;
+    nvs_open("storage", NVS_READWRITE, &nvs_handler);
+
+    nvs_set_str(nvs_handler, _EEPROM_KEY, content);
+    nvs_commit(nvs_handler);
+
+    const char *response = "String was written to the EEPROM";
+    httpd_resp_send(req, response, strlen(response));
+    return ESP_OK;
 }
 
 esp_err_t HttpServer::_readStringFromEEPROM(httpd_req_t *req) 
 {  
+    nvs_handle_t nvs_handler;
+    nvs_open("storage", NVS_READONLY, &nvs_handler);
+    
+    size_t required_size = 0;
+    nvs_get_str(nvs_handler, _EEPROM_KEY, NULL, &required_size);
+    char *data = static_cast<char*>(malloc(required_size));
+    nvs_get_str(nvs_handler, _EEPROM_KEY, data, &required_size);
+
+    httpd_resp_send(req, data, sizeof(data) - 1);
+
+    ESP_LOGI(TAG, "Sending data: %s", data);
+    return ESP_OK;
 }
