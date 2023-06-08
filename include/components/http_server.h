b4aa6ad6858d4eb11d6cf8ab032707d2c2c68d73
Conficiuskyn <Moustaphcsk@gmail.com>
2023-06-08 02:43:54 +0200

    .h and .cpp file added + .h initial implementation

---
+++ include/components/http_server.h
@@ -0,0 +1,15 @@
+#include "esp_http_server.h"
+#include "esp_log.h"
+
+class HttpServer 
+{
+    public:
+        isRunning();
+        void start();
+        void stop();
+        static constexpr const char* TAG = "[http_server]";
+    
+    private:
+        bool _running{false};
+        httpd_handle_t _server;
+};
